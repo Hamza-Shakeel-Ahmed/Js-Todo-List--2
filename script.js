816e045143be7739135788875929c8dc00228f9e
Hamza-Shakeel-Ahmed <hamzashakeelahmed460@gmail.com>
2026-01-28 18:21:53 +0500

    updated

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ const list = document.getElementById("listContainer");
 const Day = document.getElementById("Day");
 const Date = document.getElementById("Date");
 const cancelBtn = document.getElementById("cancelBtn");
-const taskText = document.querySelector("taskText");
+const filterByTask = document.getElementById("filterByTask");
+const filterByDate = document.getElementById("filterByDate");
+const li = document.getElementsByTagName("li");
+const p = document.getElementsByTagName("p");
+const dateInput = document.getElementsByClassName("date");
+
+let taskText = null;
 let editedIndex = null;
 
 const todos = [];
@@ -47,9 +53,9 @@ function render() {
       ? (newList.className = "text-decoration-line-through")
       : (newList.className = "text-decoration-none");
     newList.innerHTML = `
-    <div class=" flex-wrap flex-grow-1 taskText"><p class="col-8 mt-1 text-truncate">${val.task}</p></div>
+    <div class=" flex-wrap flex-grow-1 "><p class="taskText col-8 mt-1 text-truncate">${val.task}</p></div>
     <div>${val.Day}</div>
-    <div class= "mx-4 text-nowrap ">${val.Date}</div>
+    <div class= "mx-4 text-nowrap date ">${val.Date}</div>
     <button onclick = "editTask(${index})" ><i class="bi bi-pencil-square"></i></button>
     <button onclick = "deleteTask(${index})"><i class="bi bi-trash3-fill"></i></button>
     </li>
@@ -58,7 +64,9 @@ function render() {
     icon.addEventListener("click", function (e) {
       checkBox(index);
     });
+
     list.appendChild(newList);
+    taskText = val.task;
   });
   Day.value = "Monday";
   Date.value = "";
@@ -90,4 +98,39 @@ function checkBox(index) {
 function cancelEditing() {
   cancelBtn.classList.add("d-none");
   addTask();
+}
+
+function FilterByTask() {
+  filterByTask.addEventListener("input", (e) => {
+    let inputValue = e.target.value;
+
+    for (let i = 0; i < p.length; i++) {
+      if (!p[i].innerHTML.includes(inputValue)) {
+        li[i].style.display = "none";
+      } else {
+        li[i].style.display = "flex";
+      }
+    }
+  });
+}
+function FilterByDate() {
+  filterByDate.addEventListener("input", (e) => {
+    let inputValue = e.target.value;
+
+    for (let i = 0; i < dateInput.length; i++) {
+      if (
+        !dateInput[i].innerHTML.includes(inputValue) &&
+        !p[i].innerHTML.includes(FilterByTask.value)
+      ) {
+        li[i].style.display = "none";
+      } else if (
+        p[i].innerHTML.includes(FilterByTask.value) &&
+        !dateInput[i].innerHTML.includes(inputValue)
+      ) {
+        li[i].style.display = "flex";
+      } else if (dateInput[i].innerHTML.includes(inputValue)) {
+        li[i].style.display = "flex";
+      }
+    }
+  });
 }
